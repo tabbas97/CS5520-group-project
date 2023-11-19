82ae8e81c1b71cf04c394a0bc5744706cc24d17d
kaiqiz8 <kzhang49@binghamton.edu>
2023-11-18 22:25:27 -0500

    connect to realtime database

---
+++ app/src/main/java/edu/northeastern/g15finalproject/Report.java
@@ -0,0 +1,15 @@
+package edu.northeastern.g15finalproject;
+
+public class Report {
+    public String address;
+    public String detail;
+
+    public Report() {
+
+    }
+
+    public Report(String address, String detail) {
+        this.address = address;
+        this.detail = detail;
+    }
+}
--- app/src/main/java/edu/northeastern/g15finalproject/ReportActivity.java
+++ app/src/main/java/edu/northeastern/g15finalproject/ReportActivity.java
@@ -1,8 +1,13 @@
 package edu.northeastern.g15finalproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -11,4 +16,28 @@ public class ReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
     }
+
+    public void addReport(View view) {
+        String address = ((TextView)findViewById(R.id.address_input)).getText().toString();
+        String detail = ((TextView)findViewById(R.id.report_detail_input)).getText().toString();
+
+        if (isInputEmpty(address) || isInputEmpty(detail)) {
+            showToast("Address and details cannot be empty");
+        } else {
+            Report newReport = new Report(address, detail);
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            database.getReference().child("report").push().setValue(newReport);
+
+        }
+
+    }
+
+    private boolean isInputEmpty(String input) {
+        return input.trim().isEmpty();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
